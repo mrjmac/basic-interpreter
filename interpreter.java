1807b520e14229f065932912945207a1ddc774a8
mrjmac <40571030+mrjmac@users.noreply.github.com>
2023-07-03 14:29:18 -0500

    this is wrong and doesn't recur but im saving as a checkpoint

--- interpreter.java
+++ interpreter.java
@@ -7,10 +7,11 @@
 import java.io.*;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
-import java.util.Arrays;
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class interpreter {
+
     public static void main(String args[]) throws IOException
     {
         Scanner r = new Scanner(new FileReader("code.in"));
@@ -18,7 +19,7 @@ public class interpreter {
         String raw;
         StringTokenizer st; //read input line by line
 
-        TreeMap<String, Integer> vars = new TreeMap<>();
+        vars = new TreeMap<>();
 
         while (r.hasNext())
         {
@@ -30,28 +31,27 @@ public class interpreter {
             // we can do two things, print and assign. If we print, the first word in a line will be print
             if (token.equals("print"))
             {
-                if (raw.indexOf(".") != (raw.length() - 1) || raw.indexOf(".") == -1)
-                {
-                    pw.println("Syntax error");
-                    break;
-                }
-                else
-                {
-                    pw.println(handleOps(vars, raw.substring(6, raw.length())));
-                }
+                tokens = new StringTokenizer(raw.substring(6));
+                currToken = tokens.nextToken();
+                    
+                pw.println(expression());
+
             }
             else
             {
                 String name = token;
+                //TODO: name can't start with a number and can't be "END"
                 token = st.nextToken();
                 if (!token.equals("="))
                 {
                     pw.println("Syntax error");
-                    break;
+                    System.exit(0);
                 }
                 else
                 {
-                    int num = handleOps(vars, raw.substring(name.length() + 3, raw.length()));
+                    tokens = new StringTokenizer(raw.substring(name.length() + 3));
+                    currToken = tokens.nextToken();
+                    int num = expression();
                     vars.put(name, num);
                 }
             }
@@ -62,9 +62,117 @@ public class interpreter {
         r.close();
     }
 
-    public static int handleOps(TreeMap<String, Integer> vars, String text)
+    public static String currToken;
+    public static TreeMap<String, Integer> vars;
+    public static StringTokenizer tokens;
+
+    public static int expression()
     {
-        System.out.println(text);
+        if (tokens.hasMoreTokens())
+        {
+            return parse();
+        }
+        else if (isNum(currToken))
+        {
+            return Integer.parseInt(currToken);
+        }
+        else
+        {
+            System.out.println("HERE");
+            System.out.println("Syntax error");
+            System.exit(0);
+        }
         return 0;
+    }
+
+    public static int parse()
+    {
+        if (currToken.equals("("))
+        {
+            currToken = tokens.nextToken();
+            expression();
+            if (currToken.equals(")"))
+            {
+                return highMath();
+            }
+            else
+            {
+                System.out.println("Syntax error");
+                System.exit(0);
+            }
+        }
+        else if (isNum(currToken))
+        {
+            return highMath();        
+        }
+        else
+        {
+            System.out.println("Syntax error");
+            System.exit(0);
+        }
+
+        return 0;
+    }
+
+    public static int highMath()
+    {
+        String temp = tokens.nextToken();
+
+        if (temp.equals("*"))
+        {
+            return Integer.parseInt(currToken) * Integer.parseInt(tokens.nextToken());
+        }
+        else if (temp.equals("/"))
+        {
+            return Integer.parseInt(currToken) / Integer.parseInt(tokens.nextToken());
+        }
+        else
+        {
+            return lowMath(temp);
+        }
+    }
+
+    public static int lowMath(String temp)
+    {
+        if (temp.equals("+"))
+        {
+            return Integer.parseInt(currToken) + Integer.parseInt(tokens.nextToken());
+        }
+        else if (temp.equals("-"))
+        {
+            return Integer.parseInt(currToken) - Integer.parseInt(tokens.nextToken());
+        }
+        else
+        {
+            System.out.println("Syntax error");
+            System.exit(0);
+        }
+
+        return 0;
+    }
+
+    public static boolean isNum(String token)
+    {
+        if (vars.get(token) != null)
+        {
+            currToken = vars.get(token) + "";
+            return true;
+        }
+        else
+        {
+            try 
+            { 
+                Integer.parseInt(token); 
+            } 
+            catch(NumberFormatException e) 
+            { 
+                return false; 
+            } 
+            catch(NullPointerException e) 
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
