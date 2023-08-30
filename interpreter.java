fd3f917cef7ef1221795e4b279087deb5f23c54e
mrjmac <40571030+mrjmac@users.noreply.github.com>
2023-08-30 00:13:51 -0500

    fix dumb issue

--- interpreter.java
+++ interpreter.java
@@ -35,10 +35,10 @@ public class interpreter {
                     parse += token + " ";
                 }
 
-                StringTokenizer tokens = new StringTokenizer(parse);
-                String currToken = tokens.nextToken();
+                StringTokenizer newTokens = new StringTokenizer(parse);
+                String newCurrToken = newTokens.nextToken();
 
-                int valid = equals(currToken, tokens);
+                int valid = parser.parse(newTokens, newCurrToken, vars);
                 
                 if (valid != 0)
                 {
@@ -67,10 +67,10 @@ public class interpreter {
                     parse += token + " ";
                 }
 
-                StringTokenizer tokens = new StringTokenizer(parse);
-                String currToken = tokens.nextToken();
+                tokens = new StringTokenizer(parse);
+                currToken = tokens.nextToken();
 
-                handleLine(currToken, tokens);
+                handleLine();
             }
             
         }
@@ -79,16 +79,16 @@ public class interpreter {
     
 
     // turn into args
-    //public static String currToken;
+    public static String currToken;
     public static TreeMap<String, Integer> vars;
-    //public static StringTokenizer tokens;
+    public static StringTokenizer tokens;
 
     public static void handleBracket()
     {
 
     }
     
-    public static void handleLine(String currToken, StringTokenizer tokens)
+    public static void handleLine()
     {
         if (currToken.equals("print"))
         {
@@ -99,17 +99,17 @@ public class interpreter {
                 parse += currToken + " ";
             }
 
-            tokens = new StringTokenizer(parse);
-            currToken = tokens.nextToken();
+            StringTokenizer newTokens = new StringTokenizer(parse);
+            String newCurrToken = newTokens.nextToken();
                     
-            System.out.println(equals(currToken, tokens));
+            System.out.println(parser.parse(newTokens, newCurrToken, vars));
 
         }
         else
         {
             String name = currToken;
 
-            if (isNum(name.charAt(0) + ""))
+            if (parser.isNum(name.charAt(0) + ""))
             {
                 System.out.println("Syntax error");
                 System.exit(0);
@@ -131,198 +131,13 @@ public class interpreter {
                     parse += currToken + " ";
                 }
 
-                tokens = new StringTokenizer(parse);
-                currToken = tokens.nextToken();
+                StringTokenizer newTokens = new StringTokenizer(parse);
+                String newCurrToken = newTokens.nextToken();
 
-                int num = equals(currToken, tokens);
+                int num = parser.parse(newTokens, newCurrToken, vars);
                 vars.put(name, num);
             }
         }
     }
 
-    public static int equals(String currToken, StringTokenizer tokens)
-    {
-        int left = or(currToken, tokens);
-
-        while (currToken.equals("=="))
-        {
-            currToken = tokens.nextToken();
-            int right = or(currToken, tokens);
-
-            if (left != right)
-            {
-                left = 0;
-
-            }
-            else
-            {
-                if (left == 0)
-                {
-                    left = 1;
-                }
-            }
-        }
-
-        return left;
-    }
-
-    public static int or(String currToken, StringTokenizer tokens)
-    {
-        int left = xor(currToken, tokens);
-
-        while (currToken.equals("|"))
-        {
-            currToken = tokens.nextToken();
-            left = left | xor(currToken, tokens);
-        }
-
-        return left;
-    }
-
-    public static int xor(String currToken, StringTokenizer tokens)
-    {
-        int left = and(currToken, tokens);
-
-        while (currToken.equals("^"))
-        {
-            currToken = tokens.nextToken();
-            left = left ^ and(currToken, tokens);
-        }
-
-        return left;
-    }
-    
-    public static int and(String currToken, StringTokenizer tokens)
-    {
-        int left = shift(currToken, tokens);
-
-        while (currToken.equals("&"))
-        {
-            currToken = tokens.nextToken();
-            left = left & shift(currToken, tokens);
-        }
-
-        return left;
-    }
-
-    public static int shift(String currToken, StringTokenizer tokens)
-    {
-        int left = add(currToken, tokens);
-
-        while (currToken.equals(">>") || currToken.equals("<<"))
-        {
-            if (currToken.equals(">>"))
-            {
-                currToken = tokens.nextToken();
-                left = left >> add(currToken, tokens);
-            }
-            else
-            {
-                currToken = tokens.nextToken();
-                left = left << add(currToken, tokens);
-            }
-            
-        }
-
-        return left;
-    }
-
-    public static int add(String currToken, StringTokenizer tokens)
-    {
-        int left = multi(currToken, tokens);
-
-        while (currToken.equals("+") || currToken.equals("-"))
-        {
-            if (currToken.equals("+"))
-            {
-                currToken = tokens.nextToken();
-                left += multi(currToken, tokens);
-            }
-            else
-            {
-                currToken = tokens.nextToken();
-                left -= multi(currToken, tokens);
-            }
-            
-        }
-
-        return left;
-    }
-
-    public static int multi(String currToken, StringTokenizer tokens)
-    {
-        int left = parse(currToken, tokens);
-        while (currToken.equals("*") || currToken.equals("/"))
-        {
-            if (currToken.equals("*"))
-            {
-                currToken = tokens.nextToken();
-                left *= parse(currToken, tokens);
-            }
-            else
-            {
-                currToken = tokens.nextToken();
-                left /= parse(currToken, tokens);
-            }
-        }
-
-        return left;
-    }
-
-    public static int parse(String currToken, StringTokenizer tokens)
-    {
-        if (currToken.equals("("))
-        {
-            currToken = tokens.nextToken();
-            int ans = add(currToken, tokens);
-            if (tokens.hasMoreElements())
-            {
-                currToken = tokens.nextToken();
-            }
-            return ans;
-        }
-        else if (isNum(currToken))
-        {
-            int temp = Integer.parseInt(currToken);
-            if (tokens.hasMoreElements())
-            {
-                currToken = tokens.nextToken();
-            }
-            return temp;
-        }
-        else if (vars.get(currToken) != null)
-        {
-            int temp = vars.get(currToken);
-            if (tokens.hasMoreElements())
-            {
-                currToken = tokens.nextToken();
-            }
-            return temp;
-        }
-        else
-        {
-            System.out.println("Syntax Error");
-            System.exit(0);
-        }
-
-        return 0;
-    }
-    
-
-    public static boolean isNum(String token)
-    {
-        try 
-        { 
-            Integer.parseInt(token); 
-        } 
-        catch(NumberFormatException e) 
-        { 
-            return false; 
-        } 
-        catch(NullPointerException e) 
-        {
-            return false;
-        }
-        return true;
-    }
 }
---
+++ parser.java
@@ -0,0 +1,205 @@
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class parser {
+
+    public static String currToken;
+    public static TreeMap<String, Integer> vars;
+    public static StringTokenizer tokens;
+
+    public static int parse(StringTokenizer more, String curr, TreeMap<String, Integer> var)
+    {
+        currToken = curr;
+        vars = var;
+        tokens = more;
+
+        return equals();
+    }
+
+    public static int equals()
+    {
+        int left = or();
+
+        while (currToken.equals("=="))
+        {
+            currToken = tokens.nextToken();
+            int right = or();
+
+            if (left != right)
+            {
+                left = 0;
+
+            }
+            else
+            {
+                if (left == 0)
+                {
+                    left = 1;
+                }
+            }
+        }
+
+        return left;
+    }
+
+    public static int or()
+    {
+        int left = xor();
+
+        while (currToken.equals("|"))
+        {
+            currToken = tokens.nextToken();
+            left = left | xor();
+        }
+
+        return left;
+    }
+
+    public static int xor()
+    {
+        int left = and();
+
+        while (currToken.equals("^"))
+        {
+            currToken = tokens.nextToken();
+            left = left ^ and();
+        }
+
+        return left;
+    }
+    
+    public static int and()
+    {
+        int left = shift();
+
+        while (currToken.equals("&"))
+        {
+            currToken = tokens.nextToken();
+            left = left & shift();
+        }
+
+        return left;
+    }
+
+    public static int shift()
+    {
+        int left = add();
+
+        while (currToken.equals(">>") || currToken.equals("<<"))
+        {
+            if (currToken.equals(">>"))
+            {
+                currToken = tokens.nextToken();
+                left = left >> add();
+            }
+            else
+            {
+                currToken = tokens.nextToken();
+                left = left << add();
+            }
+            
+        }
+
+        return left;
+    }
+
+    public static int add()
+    {
+        int left = multi();
+
+        while (currToken.equals("+") || currToken.equals("-"))
+        {
+            if (currToken.equals("+"))
+            {
+                currToken = tokens.nextToken();
+                left += multi();
+            }
+            else
+            {
+                currToken = tokens.nextToken();
+                left -= multi();
+            }
+            
+        }
+
+        return left;
+    }
+
+    public static int multi()
+    {
+        int left = parse();
+        while (currToken.equals("*") || currToken.equals("/"))
+        {
+            if (currToken.equals("*"))
+            {
+                currToken = tokens.nextToken();
+                left *= parse();
+            }
+            else
+            {
+                currToken = tokens.nextToken();
+                left /= parse();
+            }
+        }
+
+        return left;
+    }
+
+    public static int parse()
+    {
+        if (currToken.equals("("))
+        {
+            currToken = tokens.nextToken();
+            int ans = add();
+            if (tokens.hasMoreElements())
+            {
+                currToken = tokens.nextToken();
+            }
+            return ans;
+        }
+        else if (isNum(currToken))
+        {
+            int temp = Integer.parseInt(currToken);
+            if (tokens.hasMoreElements())
+            {
+                currToken = tokens.nextToken();
+            }
+            return temp;
+        }
+        else if (vars.get(currToken) != null)
+        {
+            int temp = vars.get(currToken);
+            if (tokens.hasMoreElements())
+            {
+                currToken = tokens.nextToken();
+            }
+            return temp;
+        }
+        else
+        {
+            System.out.println("Syntax Error");
+            System.exit(0);
+        }
+
+        return 0;
+    }
+    
+
+    public static boolean isNum(String token)
+    {
+        try 
+        { 
+            Integer.parseInt(token); 
+        } 
+        catch(NumberFormatException e) 
+        { 
+            return false; 
+        } 
+        catch(NullPointerException e) 
+        {
+            return false;
+        }
+        return true;
+    }
+    
+}
